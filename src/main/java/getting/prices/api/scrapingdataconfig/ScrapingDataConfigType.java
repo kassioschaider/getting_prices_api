7f147327dddfce8d453e46232dde9e4082a2be4e
Kassio Schaider <kassio.schaider@hotmail.com>
2023-11-06 02:37:59 -0300

    Added test configuration end-point

--- src/main/java/getting/prices/api/scrapingdataconfig/ScrapingDataConfigType.java
+++ src/main/java/getting/prices/api/scrapingdataconfig/ScrapingDataConfigType.java
@@ -2,6 +2,6 @@ package getting.prices.api.scrapingdataconfig;
 
 public enum ScrapingDataConfigType {
 
-    SINGLE_PATTERN,
-    MULTI_PATTERN
+    PRICE_AND_TAG_SELLER_NAME_BY_ATTRIBUTE_AND_VALUE
+
 }
